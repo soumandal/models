cbfcc27c1761a793a3d36cfbfc6c3532e0e7435a
limuos <soumil.mandal@gmail.com>
2018-04-16 01:33:22 +0530

    added char seq2seq evaluate

--- char_seq2seq/evaluate.py
+++ char_seq2seq/evaluate.py
@@ -1,60 +1,87 @@
-from nltk.tokenize import word_tokenize
-from keras.models import load_model
+from keras.models import Model, load_model
+from keras.layers import Input
 import numpy as np
+import pickle
+import time
 
 
-def encode(word):
+def evaluate(word):
 
-    encoding_dict = {
-        'a': 1, 'b': 2, 'c': 3, 'd': 4,
-        'e': 5, 'f': 6, 'g': 7, 'h': 8,
-        'i': 9, 'j': 10, 'k': 11, 'l': 12,
-        'm': 13, 'n': 14, 'o': 15, 'p': 16,
-        'q': 17, 'r': 18, 's': 19, 't': 20,
-        'u': 21, 'v': 22, 'w': 23, 'x': 24,
-        'y': 25, 'z': 26
-    }
-    chars = [char for char in word]
-    encoded = []
-    for char in chars:
-        encoded.append(encoding_dict[char])
-    return encoded + [0]*(15 - len(encoded))
+    LATENT_DIMENSIONS = 128
+    SAVE_NAME = 'PATH'
 
+    with open(SAVE_NAME + "\\" + "num_encoder_tokens.pkl", "rb") as a:
+        num_encoder_tokens = pickle.load(a)
+    with open(SAVE_NAME + "\\" + "num_decoder_tokens.pkl", "rb") as b:
+        num_decoder_tokens = pickle.load(b)
+    with open(SAVE_NAME + "\\" + "max_encoder_seq_length.pkl", "rb") as c:
+        max_encoder_seq_length = pickle.load(c)
+    with open(SAVE_NAME + "\\" + "max_decoder_seq_length.pkl", "rb") as d:
+        max_decoder_seq_length = pickle.load(d)
+    with open(SAVE_NAME + "\\" + "source_token_index.pkl", "rb") as e:
+        source_token_index = pickle.load(e)
+    with open(SAVE_NAME + "\\" + "target_token_index.pkl", "rb") as f:
+        target_token_index = pickle.load(f)
+    model = load_model(SAVE_NAME + "\\" + 'model.h5')
 
-def roundup(value):
-    if value <= 0.92:
-        return 0
-    else:
-        return 1
+    encoder_inputs = model.input[0]
+    encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output
+    encoder_states = [state_h_enc, state_c_enc]
+    encoder_model = Model(encoder_inputs, encoder_states)
 
+    decoder_inputs = model.input[1]
+    decoder_state_input_h = Input(shape=(LATENT_DIMENSIONS,), name='input_3')
+    decoder_state_input_c = Input(shape=(LATENT_DIMENSIONS,), name='input_4')
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_lstm = model.layers[3]
+    decoder_outputs, state_h_dec, state_c_dec = decoder_lstm(
+        decoder_inputs, initial_state=decoder_states_inputs)
+    decoder_states = [state_h_dec, state_c_dec]
+    decoder_dense = model.layers[4]
+    decoder_outputs = decoder_dense(decoder_outputs)
 
-def tag(sentence):
+    decoder_model = Model(
+        [decoder_inputs] + decoder_states_inputs,
+        [decoder_outputs] + decoder_states)
 
-    model = load_model('model.h5')
-    tokens = word_tokenize(sentence)
+    reverse_target_char_index = dict((i, char) for char, i in target_token_index.items())
 
-    tagged = []
-    for token in tokens:
-        enc = np.array([encode(token)])
-        if roundup(model.predict(enc)[0]) == 0:
-            tagged.append(token + "\\bn")
-        else:
-            tagged.append(token + "\\en")
+    source_texts = [word]
 
-    return " ".join(tagged)
+    encoder_input_data = np.zeros((len(source_texts), max_encoder_seq_length,
+                                   num_encoder_tokens), dtype='float32')
 
+    for index, source_text in enumerate(source_texts):
+        for t, char in enumerate(source_text):
+            try:
+                encoder_input_data[index, t, source_token_index[char]] = 1.
+            except IndexError:
+                pass
 
-def collect(sentence):
+    def decode_sequence(input_seq):
+        states_value = encoder_model.predict(input_seq)
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, target_token_index['\t']] = 1.
+        stop_condition = False
+        decoded_sentence = ''
+        while not stop_condition:
+            output_tokens, h, c = decoder_model.predict(
+                [target_seq] + states_value)
+            sampled_token_index = np.argmax(output_tokens[0, -1, :])
+            sampled_char = reverse_target_char_index[sampled_token_index]
+            decoded_sentence += sampled_char
+            if (sampled_char == '\n' or
+               len(decoded_sentence) > max_decoder_seq_length):
+                stop_condition = True
+            target_seq = np.zeros((1, 1, num_decoder_tokens))
+            target_seq[0, 0, sampled_token_index] = 1.
+            states_value = [h, c]
+        return decoded_sentence
 
-    model = load_model('model.h5')
-    tokens = word_tokenize(sentence)
+    output = []
+    for sequence_index in range(len(source_texts)):
+        source_sequence = encoder_input_data[sequence_index:sequence_index + 1]
+        decoded_sequence = decode_sequence(source_sequence)
+        output.append(str(decoded_sequence))
 
-    bn_words, en_words = [], []
-    for token in tokens:
-        enc = np.array([encode(token)])
-        if roundup(model.predict(enc)[0]) == 0:
-            bn_words.append(token)
-        else:
-            en_words.append(token)
-
-    return bn_words, en_words
+    return output[0]
